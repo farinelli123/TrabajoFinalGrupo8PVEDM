846b69607c997a9403ca3957e7ec669b0092a652
alvaro <alvaro@alvaro-HP-Notebook>
2020-06-15 21:25:30 -0300

    Inicialización de Trabajo Final

---
+++ src/main/java/ar/edu/unju/edm/TrabajoFinalGrupo8PvedmApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalGrupo8PvedmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinalGrupo8PvedmApplication.class, args);
+	}
+
+}
